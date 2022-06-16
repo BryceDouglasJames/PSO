c1589b6a2be7fb05d2b8891d9a4c8658b93e5f7f
BryceDouglasJames <brycejames1214@yahoo.com>
2022-06-16 03:41:19 -0400

    Rendered pixels used to generate gradient heat map

--- public/svg.js
+++ public/svg.js
@@ -1,4 +1,5 @@
- let random_data = []
+
+ let random_data = [], pixels = []
  width = window.innerWidth
  height = window.innerHeight
 
@@ -12,75 +13,130 @@
           });
      }
  }
-generate_Random_points()
 
- //initalize d3 svg
- var svg = d3.select("svg").attr("width", width).attr("height", height);
-svg.style("display", "block")
-svg.style("margin", "auto")
-svg.style("background-color", "pink")
+/*var Rainbow = require("rainbowvis.js");
+let gradient = new Rainbow()
+gradient.setNumberRange(Math.min(pixels), Math.max(pixels))
+gradient.setSpectrum("pink", 'white')
+function generate_pixels(){
+     pixels = []
+     let hex_string = ''
+     for(let y = 0; y <= window.innerHeight; y+=5){
+          for(let x = 0; x <= window.innerWidth; x+=5){
+               func_val = (x**2) + ((y+1)**2) -(5*Math.cos(1.5*x+1.5)) -(3*Math.cos(2*x - 1.5))
+               hex_string =  graident.colorAt(func_val)
+               pixels.push({
+                    //the weight will be the id based on the defined function 
+                    id: "#" + hex_string,
+                    x: x,
+                    y: y,
+               });
+          }
+     }
+ }*/
+async function display(){
+     await fetch('/gradient')
+          .then(response => {
+               response.json().then(res_arr=>({
+                    ret_arr: res_arr
+               })     
+          ).then(res=>{
+               pixels = res.ret_arr
+               //initalize d3 svg
+               var svg = d3.select("svg").attr("width", width).attr("height", height);
+               svg.style("display", "block")
+               svg.style("margin", "auto")
+
+               var simulation = d3.forceSimulation()
+               .force("center", d3.forceCenter(width/2, height/2))
+               .force("charge", d3.forceManyBody())
+               .force("x", d3.forceX(width/2).strength(0.3))
+               .force("y", d3.forceY(height/2).strength(0.3))
+
+               var pixel = svg.append('g')
+                    .attr('class', 'pixels')
+                    .selectAll('rect')
+                    .data(pixels)
+                    .enter().append('rect')
+                         .attr('x', function(d){ return d.x })
+                         .attr('y', function(d){ return d.y })
+                         .attr('width', 3)
+                         .attr('height', 3)
+                         .attr('fill', function(d){ return d.id })
+
+               var node = svg
+                    .append('g')
+                    .attr('class', 'nodes')
+                    .selectAll('circle')
+                    .data(random_data)
+                    .enter().append('circle')
+                         .attr('r', 7)
+                         .attr("fill", "red")
+                         .call(d3.drag()
+                              .on("start", dragstarted)
+                              .on("drag", dragged)
+                              .on("end", dragended));
+                         
+               simulation.nodes(random_data)
+               simulation.on('tick', handletick);
 
 
-var simulation = d3.forceSimulation()
-    .force("center", d3.forceCenter(width/2, height/2))
-    .force("charge", d3.forceManyBody())
-    .force("x", d3.forceX(width/2).strength(0.3))
-    .force("y", d3.forceY(height/2).strength(0.3))
-    
-var node = svg
-     .append('g')
-     .attr('class', 'nodes')
-     .selectAll('circle')
-     .data(random_data)
-     .enter().append('circle')
-          .attr('r', 7)
-          .call(d3.drag()
-               .on("start", dragstarted)
-               .on("drag", dragged)
-               .on("end", dragended));
-          
-simulation.nodes(random_data)
-simulation.on('tick', handletick);
+               //event handlers
+               function handletick(){
+                    node.attr('cx', function(d) { return d.x; })
+                    node.attr('cy', function(d) { return d.y; })
+               }
 
 
-//event handlers
-function handletick(){
-     node.attr('cx', function(d) { return d.x; })
-     node.attr('cy', function(d) { return d.y; })
+               function dragstarted(e){
+                    if(!d3.event.active)
+                         simulation.alphaTarget(0.2).restart();
+                    e.fx = e.x
+                    e.fy = e.y
+               }
+
+               function dragged(e){
+                    e.fx = d3.event.x
+                    e.fy = d3.event.y
+               }
+
+               function dragended(e){
+                    if(!d3.event.active)
+                         simulation.alphaTarget(0)
+                    e.fx = null
+                    e.fy = null
+               }
+
+               /*
+               //enable zoom functionality
+               svg.call(d3.zoom()
+               .scaleExtent([1, 5])
+               //.translateExtent([[0,0], [width*2,height*2]])
+               .on('zoom', handlezoom));
+               svg.append("g")
+               function handlezoom(e){
+               console.log(d3.event.transform)
+               var center = d3.mouse(this)
+               newx = center[0] - d3.event.transform.x
+               newy = center[1] - d3.event.transform.y
+               svg.attr('transform', `translate(${newx},${newy}) scale(${d3.event.transform.k})`)
+               }
+               */
+
+
+
+
+               /*
+               display gradient
+               - generate square 1 unit length and assign it a color val DONE
+               - gradient color scale will be based between min/max values produced from function
+               - min coordinate will be assigned as glabal value
+               */
+
+               console.log(pixels)
+               return
+          })
+     })          
 }
-
-
-function dragstarted(e){
-     if(!d3.event.active)
-          simulation.alphaTarget(0.2).restart();
-     e.fx = e.x
-     e.fy = e.y
-}
-
-function dragged(e){
-     e.fx = d3.event.x
-     e.fy = d3.event.y
-}
-
-function dragended(e){
-     if(!d3.event.active)
-          simulation.alphaTarget(0)
-     e.fx = null
-     e.fy = null
-}
-
-/*
-//enable zoom functionality
-svg.call(d3.zoom()
- .scaleExtent([1, 5])
- //.translateExtent([[0,0], [width*2,height*2]])
- .on('zoom', handlezoom));
-svg.append("g")
-function handlezoom(e){
- console.log(d3.event.transform)
- var center = d3.mouse(this)
- newx = center[0] - d3.event.transform.x
- newy = center[1] - d3.event.transform.y
- svg.attr('transform', `translate(${newx},${newy}) scale(${d3.event.transform.k})`)
-}
-*/
+generate_Random_points() 
+display()
--- serve.js
+++ serve.js
@@ -1,5 +1,6 @@
 'use strict';
 const express = require('express');
+var Rainbow = require("rainbowvis.js");
 const app = express();
 const port = 5000;
 const host = '0.0.0.0';
@@ -12,6 +13,32 @@ app.use('/public', express.static(path.join(__dirname, "public")))
 app.get('/', (_, res) =>{
     res.sendFile('index.html', {root: __dirname});
 });
+
+app.get('/gradient', (_, res) =>{
+    let gradient = new Rainbow()
+    gradient.setNumberRange(1, 4000000)
+    gradient.setSpectrum("pink", 'blue')
+    let pixels = []
+        let hex_string = ''
+        for(let y = 0; y <= 2000; y+=5){
+            for(let x = 0; x <= 2000; x+=5){
+                let func_val = (x**2) + ((y+1)**2)
+                hex_string =  gradient.colorAt(func_val)
+                pixels.push({
+                        //the weight will be the id based on the defined function 
+                        id: "#" + hex_string,
+                        x: x,
+                        y: y,
+                });
+            }
+        }
+        //gradient.setNumberRange(Math.min(pixels), Math.max(pixels))
+        
+    
+    //generate_pixels()
+    //console.log(pixels)
+    res.json(pixels)
+})
 
 app.listen(port, host, ()=>{
     console.log(`Running on http://${host}:${port}`)
